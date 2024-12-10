f8775bb2e61328398319b9dd16d979e0a559f554
NekoWs <50663849+NekoWs@users.noreply.github.com>
2024-12-10 22:22:05 +0800

    优化翻译

--- js/i18n.js
+++ js/i18n.js
@@ -3,8 +3,8 @@ let languages = {
         "welcome": "欢迎",
         "input-name": "请输入你的昵称：",
         "reset": "重置",
-        "reset-message": "注意！你是否想要重置房间？",
-        "now-in": "当前房间：",
+        "reset-message": "请确认是否要重置房间？",
+        "now-in": "房间号：",
         "strike-before": "碰撞次数：",
         "strike-after": "/10",
         "join-title": "加入一个新房间？",
@@ -15,7 +15,7 @@ let languages = {
         "joined-room": "已加入房间：%s",
         "another-login": "已在另一个客户端登录",
         "warn": "警告",
-        "leave-message": "注意！你是否想离开房间？",
+        "leave-message": "请确认是否要离开房间？",
         "leaved-room": "已离开房间",
         "player-leaved": "玩家 {0} 离开了房间！",
         "notice": "提示",
@@ -26,23 +26,23 @@ let languages = {
         "cannot-connect": "无法连接至服务器！",
         "error": "错误",
         "no-player": "无玩家",
-        "invite-title": "邀请你的伙伴扫描下方二维码或复制下列链接来加入游戏！"
+        "invite-title": "扫描二维码或复制链接来加入！"
     },
     "en-US": {
         "welcome": "Welcome",
-        "input-name": "Please input your nick: ",
+        "input-name": "Please key in your nickname: ",
         "reset": "Reset",
-        "reset-message": "Attention! Do you want to reset the room?",
-        "now-in": "Current room: ",
+        "reset-message": "Do you confirm to reset the room?",
+        "now-in": "Room ID: ",
         "strike-before": "Strike count: ",
         "strike-after": "/10",
         "join-title": "Another room?",
         "join-message": "Please input the room id: ",
         "disconnected": "Disconnected",
         "disconnected-message": "Disconnected from server...",
-        "another-login": "Another login.",
+        "another-login": "Another client login.",
         "warn": "Warn",
-        "leave-message": "Attention! Do you want to leave the room?",
+        "leave-message": "Do you confirm to leave the room?",
         "leaved-room": "Leaved the room.",
         "player-leaved": "Player {0} leaved.",
         "notice": "Notice",
@@ -53,7 +53,7 @@ let languages = {
         "cannot-connect": "Cannot connect to server!",
         "error": "Error",
         "no-player": "No Player",
-        "invite-title": "Invite your friends to scan the QR code below or copy the link below to join the game!"
+        "invite-title": "Use the QR or link to join!"
     }
 }
 String.prototype.format = function() {
